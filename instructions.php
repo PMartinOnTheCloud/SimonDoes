2c9577cfab526b118a98bb3fda48945f695060d7
kokodemo <xnominatorxx@gmail.com>
2020-10-21 19:28:01 +0200

    Creation of instructions.php

---
+++ instructions.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php echo"<h1>Welcome to Simon Says! </h1>";
+	echo"<p>To start the game just PRESS the 'PLAY!'button just like in the image below: </p>";
+	echo"<img src="">"
+	echo"<button> PLAY! </button>"?>;
+
+
+</body>
+</html>
