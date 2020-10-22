60cc5162d686360bfade7a53c8e46b997ba410a9
kokodemo <xnominatorxx@gmail.com>
2020-10-22 16:48:20 +0200

    instructions finish 22/10/2020 16:50

--- instructions.php
+++ instructions.php
@@ -2,12 +2,36 @@
 <html>
 <head>
 	<title></title>
+	<style type="text/css">
+		mark.important {
+			
+			font-weight: bold;
+			background-color:cyan;
+			
+		}
+		mark.correct{
+			font-weight: bold;
+			background-color:green;
+		}
+		mark.incorrect{
+			font-weight: bold;
+			background-color:red;
+		}
+/*Por ejemplo, cambiar con paleta los colores.*/
+	</style>
 </head>
 <body>
 	<h1>Welcome to Simon Says! </h1>
-	<p>To start the game just PRESS the <mark>PLAY!</mark> button just like in the image below: </p>
-	<img src='./Images/play.jpg' width='250px' heigth='150px'><br><br><br>
-	<button> PLAY! </button>
+	<p>To start the game just PRESS the <mark class="important">PLAY!</mark> button.
+	<p>When the game starts a table box will appear, after that a few boxes will light up, to <mark class="correct">win the game you need to press the correct boxes. </p>
+	<p>But <mark class="important">be carefull!</mark> If you fail a single one you will <mark class="incorrect">lose the game!</mark></p>
+	<p>Win multiple games to increase the dificult of it by increasing the speed of the boxes to disapear and the number of it.</p>
+	<p>If you understanded the instructions please press "CONTINUE".</p>
+<form action="home.php" method="get">
+    <button type="submit" />CONTINUE
+</form>
+
+
 
 </body>
 </html>
