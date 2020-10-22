44448d031e13c8ecbbeb7f21c91ec3e28e599f3b
Pradas137 <pradas137@outlook.es>
2020-10-22 19:45:25 +0200

    Modificar css and win gameOver

--- gameover.php
+++ gameover.php
@@ -1,10 +1,32 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>GameOver</title>
-	<link rel="stylesheet" type="text/css" href="CSS/gameover.css">
-</head>
-<body>
-	<p>GAME OVER</p>
+<meta charset="utf-8">	
+<link href="CSS/gameover.css" rel="stylesheet" type="text/css" />	
+
+<title>GameOver</title>
+
+ </head>
+
+ <body>
+
+<div id="gameover">
+
+	<div class="gameover">
+	<p> GAME </p>
+	<p> OVER </p>
+	</div>
+
+	<div class="continue"> <p> CONTINUE? </p> </div>
+
+	<div class="opcoes">
+		<div class="yes"> <a href="to_play.php"> YES </a> </div>
+		<div class="no"> <a href="Home.php"> NO </a> </div>
+	</div>
+
+</div>
 </body>
+
+
 </html>
