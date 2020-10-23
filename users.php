b777bf208756df7ceab2682fcff8767a020c3891
Pradas137 <pradas137@outlook.es>
2020-10-23 18:01:07 +0200

    user

--- users.php
+++ users.php
@@ -18,20 +18,29 @@
     <!DOCTYPE html>
     <html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>
        <head>
-         <meta http-equiv='content-type' content='text/html;charset=utf-8' />
-         <title>Login</title>
+        <meta http-equiv='content-type' content='text/html;charset=utf-8' />
+        <title>Login</title>
+        <link rel="stylesheet" type="text/css" href="CSS/user.css">
        </head>
+
        <body>
+        <div class="header">
+          <a class="Logo">SimonDoes</a>
+        </div>
+        <div class="form">
         <form name='input' action='Home.php? name=username' method='post'>
           <label for='username'> User:</label>
             <input type='text' value='' id='username' name='username'  required autofocus/>
             <div class='error'><?php echo $userError;?></div>
-            <label for='password'>Password:</label>
+            <label for='password'>Pass:</label>
             <input type='password' value='' id='password' name='password' required/>
             <div class='error'><?php echo $passError;?></div>
         <input type='submit' value='Home' name='sub' />
       </form>
-
+      </div>
+      <div class="footer">
+        <p>Adrian Pradas - Carlos - Pablo </p>
+      </div>
     </body>
     </html>
 
