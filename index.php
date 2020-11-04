7196209f4066dd3f123cbb5c66d1c162f8f96ce2
Pradas137 <55705052+Pradas137@users.noreply.github.com>
2020-11-04 22:34:14 +0100

    index

--- index.php
+++ index.php
@@ -5,30 +5,37 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
-	<title>Instructions</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="CSS/Index.css">
-	
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <LINK REL="stylesheet" TYPE="text/css" HREF="CSS/Index.css">
 </head>
+ 
 <body>
-	<div class="header">
-		<h1 class="Logo">Welcome to SimonDone! </h1>
-	</div>
-	<div id="wrapper" class="clearfix">
+ 
+<div class="container">
+ 
+ <!-- Empieza Header -->
+  <header>
+    <h1 class="Logo">Welcome to SimonDone! </h1>
+  </header>
+<!-- Finaliza Header -->
 
-		<div id="first">	
-		<p id="text">To start the game just press the <mark class="important">START</mark> button.
-		<p id="text">When the game starts a table box will appear, after that a few boxes will light up. </p>
-		<p id="text"><mark class="correct">To win the game you need to press the correct boxes. </p>
-		<h3 id="text">But <mark class="important">be carefull!</mark> If you fail a single one you will <mark class="incorrect">lose the game!</mark></h3>
-		<p id="text">Win multiple games to increase the dificult of it by increasing the speed of the boxes to disapear and the number of it.</p>	
-		<p id="text">If you understood the instructions, enter a username and press <mark class="important"><strong>"CONTINUE".</mark></strong></p><br>	
-		</div>
-
-    <div id="second">
-      <div class="imgcontainer">
+<!-- Empieza Explicacion -->
+  <section>
+    <p id="text">To start the game just press the <mark class="important">START</mark> button.
+    <p id="text">When the game starts a table box will appear, after that a few boxes will light up. </p>
+    <p id="text"><mark class="correct">To win the game you need to press the correct boxes. </p>
+    <h3 id="text">But <mark class="important">be carefull!</mark> If you fail a single one you will <mark class="incorrect">lose the game!</mark></h3>
+    <p id="text">Win multiple games to increase the dificult of it by increasing the speed of the boxes to disapear and the number of it.</p> 
+    <p id="text">If you understood the instructions, enter a username and press <mark class="important"><strong>"CONTINUE".</mark></strong></p><br>
+  </section>
+<!-- Finaliza Explicacion -->
+ 
+<!-- Empieza Form Username -->
+  <aside>
+   <div class="imgcontainer">
         <img src="Images/win.png" alt="Avatar" class="avatar">
       </div>
       <form name='input' action='to_play.php' method='post'>
@@ -45,11 +52,15 @@
       <form action="ranking.php">
         <button id="ranking" type="submit">Ranking</button>
       </form>
-    </div>
-  </div>
-	<div class="footer">
-		<p class="foot">Creator: Adrian Pradas - Carlos Jurado - Pablo Martin </p>
-	</div>
-<script src="JS/hotkey_index.js" type="text/javascript"></script>
+    </aside>
+<!-- Finaliza Form Username -->
+ 
+<!-- Empieza Footer -->
+  <footer>
+    <p class="foot">Creator: Adrian Pradas - Carlos Jurado - Pablo Martin </p>
+  </footer>
+<!-- Empieza Footer -->
+ 
+</div>
 </body>
 </html>
