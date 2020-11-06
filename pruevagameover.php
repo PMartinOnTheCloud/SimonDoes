db275ae8e922295fc16529b524e33a2cb7abc2c8
Pradas137 <55705052+Pradas137@users.noreply.github.com>
2020-11-06 10:32:30 +0100

    ranking

--- pruevagameover.php
+++ pruevagameover.php
@@ -3,6 +3,7 @@
 
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="CSS/pruevagameover.css" rel="stylesheet" type="text/css" />	
 <link href='https://fonts.googleapis.com/css?family=Black+Ops+One' rel='stylesheet' type='text/css'>
 <title>GameOver</title>
