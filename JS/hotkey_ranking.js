5e22899a81a136965aa2046222123b741b560200
PMartinOnTheCloud <57746917+PMartinOnTheCloud@users.noreply.github.com>
2020-11-09 18:49:02 +0100

    Update hotkey_ranking.js

--- JS/hotkey_ranking.js
+++ JS/hotkey_ranking.js
@@ -1,5 +1,7 @@
 document.onkeyup = function(key) {
-  if (key.ctrlKey && key.altKey && key.which == 72) {
-    location.replace("index.php");
+  if (key.altKey && key.which == 72) {
+    document.getElementsByClassName('Logo')[0].click();
+  } else if (key.altKey && key.which == 87){
+  	document.getElementById('colorblind').click();
   }
 };
