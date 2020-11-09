90c9ad4f91d93b14ab9eaf412bc272267b2c98c5
PMartinOnTheCloud <57746917+PMartinOnTheCloud@users.noreply.github.com>
2020-11-09 18:48:27 +0100

    Update hotkey_index.js

--- JS/hotkey_index.js
+++ JS/hotkey_index.js
@@ -1,7 +1,13 @@
 document.onkeyup = function(key) {
-  if (key.ctrlKey && key.altKey && key.which == 67) {
+  if (key.altKey && key.which == 67) {
   	document.getElementById('button').click();
-  } else if (key.ctrlKey && key.altKey && key.which == 82){
+  } else if (key.altKey && key.which == 82){
   	document.getElementById('ranking').click();
+  } else if (key.altKey && key.which == 76){
+  	document.getElementById('liarbutton').click();
+  } else if (key.altKey && key.which == 83){
+  	document.getElementById('survivalbutton').click();
+  } else if (key.altKey && key.which == 87){
+  	document.getElementById('colorblind').click();
   }
 };
