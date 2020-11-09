a9d5c7659e6312b3f026bce0fe3c4098839a3f39
Pradas137 <55705052+Pradas137@users.noreply.github.com>
2020-11-09 19:56:18 +0100

    borrado

--- JS/hotkey_to_play.js
+++
@@ -1,9 +0,0 @@
-document.onkeyup = function(key) {
-  if (key.ctrlKey && key.altKey && key.which == 72) {
-    location.replace('index.php');
-  } else if (key.ctrlKey && key.altKey && key.which == 83) {
-  	document.getElementById('buttonStart').click();
-  } else if (key.ctrlKey && key.altKey && key.which == 67) {
-  	document.getElementById('buttonCheck').click();
-  }
-};
