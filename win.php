9cb30013a07fb5394dff1dae0b3448d989f5a9b0
PMartinOnTheCloud <57746917+PMartinOnTheCloud@users.noreply.github.com>
2020-11-09 18:57:13 +0100

    Update win.php

--- win.php
+++ win.php
@@ -1,30 +1,39 @@
 <?php session_start();
  if (isset($_SESSION["code"])){
         $code = $_SESSION['code'];
-    }?>
+    }
+
+if(!empty($_SESSION['visited_pages'])) {
+  $_SESSION['visited_pages']['prev'] = $_SESSION['visited_pages']['current'];
+}else {
+  $_SESSION['visited_pages']['prev'] = 'No previous page';
+}
+$_SESSION['visited_pages']['current'] = $_SERVER['REQUEST_URI'];
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="CSS/win.css" rel="stylesheet" type="text/css" />	
-<link href='https://fonts.googleapis.com/css?family=Black+Ops+One' rel='stylesheet' type='text/css'>
+<link href="CSS/win.css" rel="stylesheet" type="text/css" />
 <title>Win</title>
 
  </head>
  <?php
-
-$_SESSION['points'] += 100;
+$url = $_SESSION['visited_pages']['prev'];
+$urlexplode = explode("/", $url);
+if (end($urlexplode)== "to_play.php"){
+	$_SESSION['points'] += 100;
+	$_SESSION['level'] +=1;
+}
 
 ?>
 <body>
 	<div id="Win">
 	<div id="Header">
-		<a class="Logo">SimonDoes</a>
-		<div class="Header-right">
-			<a class="Active" href="index.php">Home</a>
-		</div>
+		<a class="Logo" href="index.php">SimonDoes</a>
+		<button id="colorblind" onclick="">S<u>w</u>itch</button>	
 	</div>
 	<div class="Title">
 		<div class="Win">YOU'VE WON</div>
@@ -34,23 +43,22 @@ $_SESSION['points'] += 100;
 	</div>
 
 	<div class="User"> <p>Username: <?php $user = $_SESSION['username']; echo "$user";?></p> </div>
-
 	<div class="Relative">
 		<form method="post" action="to_play.php">
-			<button class="NextLevel" id="NextLevel" name="NextLevel" type="Submit">Next Level</button>
+			<button class="NextLevel" id="NextLevel" name="NextLevel" type="Submit"><u>N</u>ext Level</button>
 		</form>
 		<form method="post" action="to_play.php">
-			<button class="TryAgain" name="TryAgain" id="TryAgain" type="Submit">Try Again</button>
+			<button class="TryAgain" name="RetryWin" id="TryAgain" type="Submit"><u>T</u>ry Again</button>
 		</form>
 		<form method="post" action="ranking.php">
-			<button class="SaveExit" id="SaveExit" name="SaveExit" type="Submit">Save/Exit</button>
+			<button class="SaveExit" id="SaveExit" name="SaveExit" type="Submit"><u>S</u>ave/Exit</button>
 		</form>
 	</div>
 	<div class="Code">
-		<p>Code:<?php echo "$code";?> </p>
+		<p>Code: <?php echo "$code";?> </p>
 	</div>
 </div>
-<script src="JS/hotkey_gameover.js" type="text/javascript"></script>
+<script src="JS/hotkey_win.js" type="text/javascript"></script>
 </body>
 
 
