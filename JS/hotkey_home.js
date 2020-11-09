851dc1be322ae1ba0167b4c604b9bbc0f8eb0f1f
PMartinOnTheCloud <57746917+PMartinOnTheCloud@users.noreply.github.com>
2020-11-09 18:48:05 +0100

    Delete hotkey_home.js

--- JS/hotkey_home.js
+++
@@ -1,9 +0,0 @@
-document.onkeyup = function(key) {
-  if (key.ctrlKey && key.altKey && key.which == 72) {
-    location.replace('index.php');
-  } else if(key.ctrlKey && key.altKey && key.which == 83) {
-  	document.getElementById('start').click();
-  }else if(key.ctrlKey && key.altKey && key.which == 82) {
-  	document.getElementById('ranking').click();
-  }
-};
