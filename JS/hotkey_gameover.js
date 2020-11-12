62941c765b42ab1488ad43be68bdbe305a3e21d7
Pradas137 <55705052+Pradas137@users.noreply.github.com>
2020-11-12 13:12:18 +0100

    Delete hotkey_gameover.js

--- JS/hotkey_gameover.js
+++
@@ -1,9 +0,0 @@
-document.onkeyup = function(key) {
-  if (key.ctrlKey && key.altKey && key.which == 72) {
-    location.replace('index.php');
-  } else if (key.ctrlKey && key.altKey && key.which == 84) {
-  	document.getElementById("no").click();
-  } else if (key.ctrlKey && key.altKey && key.which == 83) {
-  	document.getElementById("yes").click();
-  }
-};
