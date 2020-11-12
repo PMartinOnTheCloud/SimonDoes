01a6358ee637d285a2a5a61066500951bfe9adc1
Pradas137 <55705052+Pradas137@users.noreply.github.com>
2020-11-12 13:12:30 +0100

    Delete hotkey_index.js

--- JS/hotkey_index.js
+++
@@ -1,7 +0,0 @@
-document.onkeyup = function(key) {
-  if (key.ctrlKey && key.altKey && key.which == 67) {
-  	document.getElementById('button').click();
-  } else if (key.ctrlKey && key.altKey && key.which == 82){
-  	document.getElementById('ranking').click();
-  }
-};
